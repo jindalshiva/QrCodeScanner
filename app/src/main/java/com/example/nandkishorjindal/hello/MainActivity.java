7cdefee072dce2ff4784d9fae786dd878e8faac7
nandkishorjindal <jindalshivani11@gmail.com>
2018-10-07 09:34:05 +0530

    INITIAL COMMIT

--- app/src/main/java/com/example/nandkishorjindal/hello/MainActivity.java
+++ app/src/main/java/com/example/nandkishorjindal/hello/MainActivity.java
@@ -1,13 +1,66 @@
 package com.example.nandkishorjindal.hello;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Button buttonScan;
+
+
+    private IntentIntegrator qrScan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonScan = (Button) findViewById(R.id.scanbtn);
+        final Activity activity = this;
+        buttonScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator intentIntegrator = new IntentIntegrator(activity);
+                intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+                intentIntegrator.setPrompt("scan");
+                intentIntegrator.setCameraId(0);
+                intentIntegrator.setBeepEnabled(false);
+                intentIntegrator.setBarcodeImageEnabled(false);
+                intentIntegrator.initiateScan();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        if (result!=null){
+            if(result.getContents()==null){
+                Toast.makeText(getApplicationContext(),"you cancelled scanning",Toast.LENGTH_LONG).show();
+            }
+            else
+            {
+                  Toast.makeText(this,result.getContents(),Toast.LENGTH_LONG).show();
+            }
+        }
+
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
+
+
